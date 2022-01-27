318e8bf45fdffa794191e2ab978b7b0f23ba3cff
Dirk Avery <dirk.avery@gmail.com>
2022-01-27 14:21:47 -0500

    Add sts_region, use_fips_endpoint, use_dualstack_endpoint

--- internal/conns/conns.go
+++ internal/conns/conns.go
@@ -881,8 +881,12 @@ type Config struct {
 	SkipRequestingAccountId bool
 	SkipMetadataApiCheck    bool
 	S3ForcePathStyle        bool
+	STSRegion               string
 
 	TerraformVersion string
+
+	UseDualStackEndpoint bool
+	UseFIPSEndpoint      bool
 }
 
 type AWSClient struct {
@@ -1220,6 +1224,8 @@ func (c *Config) Client() (interface{}, error) {
 		StsEndpoint:                 c.Endpoints[STS],
 		Token:                       c.Token,
 		UserAgentProducts:           StdUserAgentProducts(c.TerraformVersion),
+		//UseDualStackEndpoint:        c.UseDualStackEndpoint,
+		//UseFIPSEndpoint:             c.UseFIPSEndpoint,
 	}
 
 	sess, accountID, Partition, err := awsbase.GetSessionWithAccountIDAndPartition(awsbaseConfig)
@@ -1493,7 +1499,6 @@ func (c *Config) Client() (interface{}, error) {
 		SSOConn:                           sso.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[SSO])})),
 		SSOOIDCConn:                       ssooidc.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[SSOOIDC])})),
 		StorageGatewayConn:                storagegateway.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[StorageGateway])})),
-		STSConn:                           sts.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[STS])})),
 		SupportConn:                       support.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[Support])})),
 		SWFConn:                           swf.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[SWF])})),
 		SyntheticsConn:                    synthetics.New(sess.Copy(&aws.Config{Endpoint: aws.String(c.Endpoints[Synthetics])})),
@@ -1533,6 +1538,20 @@ func (c *Config) Client() (interface{}, error) {
 	shieldConfig := &aws.Config{
 		Endpoint: aws.String(c.Endpoints[Shield]),
 	}
+
+	// Services that require different region handling
+	stsRegion := c.STSRegion
+
+	if stsRegion == "" {
+		stsRegion = c.Region
+	}
+
+	stsConfig := &aws.Config{
+		Endpoint: aws.String(c.Endpoints[STS]),
+		Region:   aws.String(stsRegion),
+	}
+
+	client.STSConn = sts.New(sess.Copy(stsConfig))
 
 	// Services that require multiple client configurations
 	s3Config := &aws.Config{
